05b76dd376c4af9a059d40e23133e0ca3427a045
Inamura JIN <60126349+InamuraJIN@users.noreply.github.com>
2020-08-22 18:42:24 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 config = {
-    "Manual_URL" : 'https://github.com/InamuraJIN/CommandRecorder/blob/master/README.md',
-    "Hint_URL": '',
-    "BugReport_URL": '',
+    "Manual_URL" : 'https://github.com/InamuraJIN/ActionRecorder/wiki',
+    "Hint_URL": 'https://github.com/InamuraJIN/ActionRecorder/wiki',
+    "BugReport_URL": 'https://twitter.com/Inamura_JIN',
     "checkSource_URL": 'https://api.github.com/repos/InamuraJIN/ActionRecorder/contents/__init__.py',
     "repoSource_URL": 'https://github.com/InamuraJIN/ActionRecorder',
     "releasNotes_URL": ''
