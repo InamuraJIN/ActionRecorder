24083a027e8618a467a737c2f10de83a5b0001db
Inamura JIN <60126349+InamuraJIN@users.noreply.github.com>
2020-10-28 15:03:46 +0900

    2.9

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from . import ActionRecorder as ActionRecorder
 bl_info = {
     "name" : "ActionRecorder",
     "author" : "InamuraJIN, Rivin",
-    "version": (3, 4, 1),
+    "version": (3, 5, 3),
     "blender": (2, 83, 0),
     "location" : "View 3D",
     "warning" : "",
