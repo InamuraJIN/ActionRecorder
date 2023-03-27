f03e8532028f9bd8cca4fb36b82c6dbd4d7e9172
RivinHD <58261670+RivinHD@users.noreply.github.com>
2023-03-27 22:29:03 +0200

    Version 4.0.7

--- ActRec/__init__.py
+++ ActRec/__init__.py
@@ -3,7 +3,7 @@
 bl_info = {
     "name": "ActionRecorder",
     "author": "InamuraJIN, Rivin",
-    "version": (4, 0, 6),
+    "version": (4, 0, 7),
     "blender": (3, 3, 5),
     "location": "View 3D",
     "warning": "",
